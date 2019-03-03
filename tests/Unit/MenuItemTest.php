fd38c4543bf96c405a91c2d38a582a4876dc1112
Abdelrahman Omran <me@omranic.com>
2019-03-03 20:13:17 +0400

    Apply PHPUnit 8 updates

--- tests/Unit/MenuItemTest.php
+++ tests/Unit/MenuItemTest.php
@@ -14,7 +14,7 @@ class MenuItemTest extends BaseTestCase
      */
     protected $menuManager;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->menuManager = app(MenuManager::class);
--- tests/Unit/MenuTest.php
+++ tests/Unit/MenuTest.php
@@ -14,7 +14,7 @@ class MenuTest extends BaseTestCase
      */
     protected $menuManager;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
